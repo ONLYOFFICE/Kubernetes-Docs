4f3815b191bdc9ce3e7b2be7408d031d13da4086
VyacheslavSemin <sla1733@yandex.ru>
2021-04-28 14:25:40 +0000

    Change the structure of the update script

--- scripts/update-ds.sh
+++ scripts/update-ds.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 DOCUMENTSERVER_VERSION=""
 LOGOUT_STR="work done"
-STATUS_STR="Completed"
 
 if [ "$1" == "" ]; then
   echo "Basic parameters are missing."
@@ -10,58 +9,77 @@ else
   DOCUMENTSERVER_VERSION=$1
 fi
 
-wget https://raw.githubusercontent.com/ONLYOFFICE/server/master/schema/postgresql/removetbl.sql
-kubectl create configmap remove-db-scripts --from-file=./removetbl.sql
-kubectl apply -f ./configmaps/update-ds.yaml
-kubectl apply -f ./jobs/prepare4update.yaml
-sleep 5
-PODNAME="$(kubectl get pod | grep -i prepare4update | awk '{print $1}')"
-
-updating(){
-  STATUS="$(kubectl get pod "${PODNAME}" |  awk '{print $3}' | sed -n '$p')"
-  LOGOUT="$(kubectl logs "${PODNAME}" | sed -n '$p')"
-  if [ "${LOGOUT}" == "$LOGOUT_STR" ]; then
-    if [ "${STATUS}" == "$STATUS_STR" ]; then
-      kubectl delete job prepare4update
-      kubectl set image deployment/spellchecker \
-        spellchecker=onlyoffice/docs-spellchecker-de:${DOCUMENTSERVER_VERSION}
-      kubectl set image deployment/converter \
-        converter=onlyoffice/docs-converter-de:${DOCUMENTSERVER_VERSION}
-      kubectl set image deployment/docservice \
-        docservice=onlyoffice/docs-docservice-de:${DOCUMENTSERVER_VERSION} \
-        proxy=onlyoffice/docs-proxy-de:${DOCUMENTSERVER_VERSION}
-    else
-      echo -e "\e[0;31m The update preparation Job has an incorrect status \e[0m"
-      kubectl delete job prepare4update
-      exit 1
-    fi
-  else
-    kubectl delete job prepare4update
-    echo -e "\e[0;31m The result of the script from Job is different from the expected one \e[0m"
+preparing_for_update(){
+  kubectl get job | grep -iq prepare4update
+  if [ $? -eq 0 ]; then
+    echo A Job named prepareupdate exists. Exit
     exit 1
+  else
+    wget https://raw.githubusercontent.com/ONLYOFFICE/server/master/schema/postgresql/removetbl.sql
+    kubectl create configmap remove-db-scripts --from-file=./removetbl.sql
+    kubectl apply -f ./configmaps/update-ds.yaml
   fi
 }
 
-while true; do
-    case $STATUS in
-        Error)
-          echo -e "\e[0;31m The update preparation Job has an incorrect status \e[0m"
-          echo $STATUS
-          kubectl delete job prepare4update
-          exit 1
-        ;;
+creating_job(){
+  kubectl apply -f ./jobs/prepare4update.yaml
+  sleep 5
+  PODNAME="$(kubectl get pod | grep -i prepare4update | awk '{print $1}')"
+}
 
-        Completed)
-          echo $STATUS
-          updating
-          echo -e "\e[0;32m The update was completed successfully. Wait until all containers with the new version of the images have the READY status \e[0m"
-          exit 0
-        ;;
+error_in_job(){
+  echo -e "\e[0;31m Now the Pod operation log will be displayed. \e[0m"
+  echo -e "\e[0;31m The Job will not be deleted automatically. Further actions to manage the Job must be performed manually. \e[0m"
+  sleep 9
+  kubectl logs "${PODNAME}"
+}
 
-        *)
-          echo $STATUS
-          sleep 5
-          STATUS="$(kubectl get pod "${PODNAME}" |  awk '{print $3}' | sed -n '$p')"
-        ;;
-    esac
-done
+deleting_job(){
+  kubectl delete job prepare4update
+}
+
+updating(){
+  kubectl set image deployment/spellchecker \
+    spellchecker=onlyoffice/docs-spellchecker-de:${DOCUMENTSERVER_VERSION}
+  kubectl set image deployment/converter \
+    converter=onlyoffice/docs-converter-de:${DOCUMENTSERVER_VERSION}
+  kubectl set image deployment/docservice \
+    docservice=onlyoffice/docs-docservice-de:${DOCUMENTSERVER_VERSION} \
+    proxy=onlyoffice/docs-proxy-de:${DOCUMENTSERVER_VERSION}
+}
+
+checking_the_status(){
+  while true; do
+  	  STATUS="$(kubectl get pod "${PODNAME}" |  awk '{print $3}' | sed -n '$p')"
+      case $STATUS in
+          Error)
+            echo -e "\e[0;31m Status of the prepare4update POD: $STATUS \e[0m"
+            error_in_job
+            exit 1
+          ;;
+
+          Completed)
+            echo Status of the prepare4update POD: $STATUS
+            LOGOUT="$(kubectl logs "${PODNAME}" | sed -n '$p')"
+            if [ "${LOGOUT}" == "$LOGOUT_STR" ]; then
+              deleting_job
+              updating
+              echo -e "\e[0;32m The Job update was completed successfully. Wait until all containers with the new version of the images have the READY status. \e[0m"
+              exit 0
+            else
+              echo -e "\e[0;31m The result of the script from Job is different from the expected one. \e[0m"
+              error_in_job
+              exit 1
+            fi
+          ;;
+
+          *)
+            echo Status of the prepare4update POD: $STATUS. Wait
+            sleep 5
+          ;;
+      esac
+  done
+}
+preparing_for_update
+creating_job
+checking_the_status
