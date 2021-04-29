77ae875025bb274202b2cb7c08a7eb53d247d9dd
VyacheslavSemin <sla1733@yandex.ru>
2021-04-29 08:44:14 +0000

    Rework the actions in the update script

--- scripts/update-ds.sh
+++ scripts/update-ds.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 DOCUMENTSERVER_VERSION=""
-LOGOUT_STR="work done"
 
 if [ "$1" == "" ]; then
   echo "Basic parameters are missing."
@@ -9,10 +8,10 @@ else
   DOCUMENTSERVER_VERSION=$1
 fi
 
-prepare_for_update(){
+init_prepare4update_job(){
   kubectl get job | grep -iq prepare4update
   if [ $? -eq 0 ]; then
-    echo A Job named prepareupdate exists. Exit
+    echo A Job named prepare4update exists. Exit
     exit 1
   else
     wget https://raw.githubusercontent.com/ONLYOFFICE/server/master/schema/postgresql/removetbl.sql
@@ -21,20 +20,35 @@ prepare_for_update(){
   fi
 }
 
-create_job(){
+create_prepare4update_job(){
   kubectl apply -f ./jobs/prepare4update.yaml
   sleep 5
   PODNAME="$(kubectl get pod | grep -i prepare4update | awk '{print $1}')"
 }
 
-error_in_job(){
-  echo -e "\e[0;31m Now the Pod operation log will be displayed. \e[0m"
-  echo -e "\e[0;31m The Job will not be deleted automatically. Further actions to manage the Job must be performed manually. \e[0m"
-  sleep 9
-  kubectl logs "${PODNAME}"
+check_prepare4update_job_status(){
+  while true; do
+      STATUS="$(kubectl get pod "${PODNAME}" |  awk '{print $3}' | sed -n '$p')"
+      case $STATUS in
+          Error)
+            echo "error"
+            break
+          ;;
+
+          Completed)
+            echo "completed"
+            break
+          ;;
+
+          *)
+            sleep 5
+          ;;
+      esac
+  done
 }
 
-delete_job(){
+delete_prepare4update_job(){
+  echo -e "\e[0;32m Status of the prepare4update POD: $JOB_STATUS. The Job will be deleted. \e[0m"
   kubectl delete job prepare4update
 }
 
@@ -48,38 +62,21 @@ update_images(){
     proxy=onlyoffice/docs-proxy-de:${DOCUMENTSERVER_VERSION}
 }
 
-check_status(){
-  while true; do
-  	  STATUS="$(kubectl get pod "${PODNAME}" |  awk '{print $3}' | sed -n '$p')"
-      case $STATUS in
-          Error)
-            echo -e "\e[0;31m Status of the prepare4update POD: $STATUS \e[0m"
-            error_in_job
-            exit 1
-          ;;
-
-          Completed)
-            echo Status of the prepare4update POD: $STATUS
-            LOGOUT="$(kubectl logs "${PODNAME}" | sed -n '$p')"
-            if [ "${LOGOUT}" == "$LOGOUT_STR" ]; then
-              delete_job
-              update_images
-              echo -e "\e[0;32m The Job update was completed successfully. Wait until all containers with the new version of the images have the READY status. \e[0m"
-              exit 0
-            else
-              echo -e "\e[0;31m The result of the script from Job is different from the expected one. \e[0m"
-              error_in_job
-              exit 1
-            fi
-          ;;
-
-          *)
-            echo Status of the prepare4update POD: $STATUS. Wait
-            sleep 5
-          ;;
-      esac
-  done
+print_error_message(){
+  echo -e "\e[0;31m Status of the prepare4update POD: $JOB_STATUS \e[0m"
+  echo -e "\e[0;31m The Job will not be deleted automatically. Further actions to manage the Job must be performed manually. \e[0m"
 }
-prepare_for_update
-create_job
-check_status
+
+init_prepare4update_job
+create_prepare4update_job
+
+echo "Getting the prepare4update POD status..."
+JOB_STATUS=$(check_prepare4update_job_status)
+if [[ "$JOB_STATUS" == "error" ]]; then
+  print_error_message
+elif [[ "$JOB_STATUS" == "completed" ]]; then
+  delete_prepare4update_job
+  update_images
+  echo -e "\e[0;32m The Job update was completed successfully. Wait until all containers with the new version of the images have the READY status. \e[0m"
+fi
+exit
