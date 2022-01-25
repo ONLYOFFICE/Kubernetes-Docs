bf4758a45e594652b1801af4099c45d507f6cc5f
VyacheslavSemin <sla1733@yandex.ru>
2022-01-25 08:20:14 +0000

    Add update using a script

--- sources/scripts/shutdown-ds.sh
+++ sources/scripts/shutdown-ds.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 PRODUCT_NAME="onlyoffice"
+POD_ALL_DS_NAME="$(kubectl get pod | grep -i docservice | awk '{print $1}')"
+declare -a POD_DS_NAME=($POD_ALL_DS_NAME)
+DB_PASSWORD="$(kubectl exec ${POD_DS_NAME[0]} -c docservice -- sh -c 'echo $DB_PWD')"
 
 while [ "$1" != "" ]; do
   case $1 in
@@ -14,27 +17,25 @@ while [ "$1" != "" ]; do
   shift
 done
 
-init_prepare4shutdown_job(){
-  kubectl get job | grep -iq prepare4shutdown
+init_prepare_ds_job(){
+  kubectl get job | grep -iq prepare-ds
   if [ $? -eq 0 ]; then
-    echo A Job named prepare4shutdown exists. Exit
+    echo A Job named prepare-ds exists. Exit
     exit 1
   else
-    kubectl delete cm remove-db-scripts init-db-scripts
-    kubectl create configmap remove-db-scripts --from-file=./removetbl.sql
-    kubectl create configmap init-db-scripts --from-file=./createdb.sql
     kubectl apply -f ./templates/configmaps/stop-ds.yaml
   fi
 }
 
-create_prepare4shutdown_job(){
+create_prepare_ds_job(){
   export PRODUCT_NAME="${PRODUCT_NAME}"
-  envsubst < ./jobs/prepare4shutdown.yaml | kubectl apply -f -
+  export DB_PASSWORD="${DB_PASSWORD}"
+  envsubst < ./jobs/prepare-ds.yaml | kubectl apply -f -
   sleep 5
-  PODNAME="$(kubectl get pod | grep -i prepare4shutdown | awk '{print $1}')"
+  PODNAME="$(kubectl get pod | grep -i prepare-ds | awk '{print $1}')"
 }
 
-check_prepare4shutdown_pod_status(){
+check_prepare_ds_pod_status(){
   while true; do
       STATUS="$(kubectl get pod "${PODNAME}" |  awk '{print $3}' | sed -n '$p')"
       case $STATUS in
@@ -55,25 +56,25 @@ check_prepare4shutdown_pod_status(){
   done
 }
 
-delete_prepare4shutdown_job(){
-  echo -e "\e[0;32m Status of the prepare4shutdown POD: $POD_STATUS. The Job will be deleted \e[0m"
-  kubectl delete job prepare4shutdown
+delete_prepare_ds_job(){
+  echo -e "\e[0;32m Status of the prepare-ds POD: $POD_STATUS. The Job will be deleted \e[0m"
+  kubectl delete job prepare-ds
 }
 
 print_error_message(){
-  echo -e "\e[0;31m Status of the prepare4shutdown POD: $POD_STATUS \e[0m"
+  echo -e "\e[0;31m Status of the prepare-ds POD: $POD_STATUS \e[0m"
   echo -e "\e[0;31m The Job will not be deleted automatically. Further actions to manage the Job must be performed manually. \e[0m"
 }
 
-init_prepare4shutdown_job
-create_prepare4shutdown_job
+init_prepare_ds_job
+create_prepare_ds_job
 
-echo "Getting the prepare4shutdown POD status..."
-POD_STATUS=$(check_prepare4shutdown_pod_status)
+echo "Getting the prepare-ds POD status..."
+POD_STATUS=$(check_prepare_ds_pod_status)
 if [[ "$POD_STATUS" == "error" ]]; then
   print_error_message
 else
-  delete_prepare4shutdown_job
+  delete_prepare_ds_job
   echo -e "\e[0;32m The Job shutdown was completed successfully.\e[0m"
 fi
 exit
--- sources/scripts/update-ds.sh
+++ sources/scripts/update-ds.sh
@@ -2,6 +2,9 @@
 DOCUMENTSERVER_VERSION=""
 PRODUCT_NAME="onlyoffice"
 PRODUCT_EDITION="de"
+POD_ALL_DS_NAME="$(kubectl get pod | grep -i docservice | awk '{print $1}')"
+declare -a POD_DS_NAME=($POD_ALL_DS_NAME)
+DB_PASSWORD="$(kubectl exec ${POD_DS_NAME[0]} -c docservice -- sh -c 'echo $DB_PWD')"
 
 if [ "$1" == "" ]; then
   echo "Basic parameters are missing."
@@ -37,29 +40,25 @@ if [ "$DOCUMENTSERVER_VERSION" == "" ]; then
   exit 1
 fi
 
-init_prepare4update_job(){
-  kubectl get job | grep -iq prepare4update
+init_prepare_ds_job(){
+  kubectl get job | grep -iq prepare-ds
   if [ $? -eq 0 ]; then
-    echo A Job named prepare4update exists. Exit
+    echo A Job named prepare-ds exists. Exit
     exit 1
   else
-    kubectl delete cm remove-db-scripts init-db-scripts
-    wget -O removetbl.sql https://raw.githubusercontent.com/ONLYOFFICE/server/master/schema/postgresql/removetbl.sql
-    wget -O createdb.sql https://raw.githubusercontent.com/ONLYOFFICE/server/master/schema/postgresql/createdb.sql
-    kubectl create configmap remove-db-scripts --from-file=./removetbl.sql
-    kubectl create configmap init-db-scripts --from-file=./createdb.sql
-    kubectl apply -f ./sources/stop-ds.yaml
+    kubectl apply -f ./templates/configmaps/stop-ds.yaml
   fi
 }
 
-create_prepare4update_job(){
+create_prepare_ds_job(){
   export PRODUCT_NAME="${PRODUCT_NAME}"
-  envsubst < ./jobs/prepare4update.yaml | kubectl apply -f -
+  export DB_PASSWORD="${DB_PASSWORD}"
+  envsubst < ./jobs/prepare-ds.yaml | kubectl apply -f -
   sleep 5
-  PODNAME="$(kubectl get pod | grep -i prepare4update | awk '{print $1}')"
+  PODNAME="$(kubectl get pod | grep -i prepare-ds | awk '{print $1}')"
 }
 
-check_prepare4update_pod_status(){
+check_prepare_ds_pod_status(){
   while true; do
       STATUS="$(kubectl get pod "${PODNAME}" |  awk '{print $3}' | sed -n '$p')"
       case $STATUS in
@@ -80,9 +79,9 @@ check_prepare4update_pod_status(){
   done
 }
 
-delete_prepare4update_job(){
-  echo -e "\e[0;32m Status of the prepare4update POD: $POD_STATUS. The Job will be deleted \e[0m"
-  kubectl delete job prepare4update
+delete_prepare_ds_job(){
+  echo -e "\e[0;32m Status of the prepare-ds POD: $POD_STATUS. The Job will be deleted \e[0m"
+  kubectl delete job prepare-ds
 }
 
 update_images(){
@@ -95,19 +94,19 @@ update_images(){
 }
 
 print_error_message(){
-  echo -e "\e[0;31m Status of the prepare4update POD: $POD_STATUS \e[0m"
+  echo -e "\e[0;31m Status of the prepare-ds POD: $POD_STATUS \e[0m"
   echo -e "\e[0;31m The Job will not be deleted automatically. Further actions to manage the Job must be performed manually. \e[0m"
 }
 
-init_prepare4update_job
-create_prepare4update_job
+init_prepare_ds_job
+create_prepare_ds_job
 
-echo "Getting the prepare4update POD status..."
-POD_STATUS=$(check_prepare4update_pod_status)
+echo "Getting the prepare_ds POD status..."
+POD_STATUS=$(check_prepare_ds_pod_status)
 if [[ "$POD_STATUS" == "error" ]]; then
   print_error_message
 else
-  delete_prepare4update_job
+  delete_prepare_ds_job
   update_images
   echo -e "\e[0;32m The Job update was completed successfully. Wait until all containers with the new version of the images have the READY status. \e[0m"
 fi
