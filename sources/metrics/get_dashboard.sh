12efe2db82b14a532b4bdc2c518ca5d3acdbd1b3
kireevdmitry <octavarium78@gmail.com>
2022-05-05 03:37:13 -0400

    Move helm repo to aws

--- sources/metrics/get_dashboard.sh
+++ sources/metrics/get_dashboard.sh
@@ -5,6 +5,7 @@ wget https://grafana.com/api/dashboards/11835/revisions/1/download -O dashboard-
 wget https://grafana.com/api/dashboards/10991/revisions/8/download -O dashboard-rabbitmq.json
 wget https://grafana.com/api/dashboards/9628/revisions/6/download -O dashboard-postgresql.json
 wget https://grafana.com/api/dashboards/9614/revisions/1/download -O dashboard-nginx-ingress.json
+wget https://github.com/ONLYOFFICE/Kubernetes-Docs/blob/master/charts/documentserver/sources/metrics/documentserver-statsd-exporter.json
 sed -i 's/${DS_PROMETHEUS}/Prometheus/' *.json
 kubectl create configmap dashboard-node-exporter --from-file=./dashboard-node-exporter.json
 kubectl create configmap dashboard-deployment --from-file=./dashboard-deployment.json
@@ -12,4 +13,4 @@ kubectl create configmap dashboard-redis --from-file=./dashboard-redis.json
 kubectl create configmap dashboard-rabbitmq --from-file=./dashboard-rabbitmq.json
 kubectl create configmap dashboard-postgresql --from-file=./dashboard-postgresql.json
 kubectl create configmap dashboard-nginx-ingress --from-file=./dashboard-nginx-ingress.json
-kubectl create configmap dashboard-documentserver --from-file=./sources/metrics/documentserver-statsd-exporter.json
+kubectl create configmap dashboard-documentserver --from-file=./documentserver-statsd-exporter.json
