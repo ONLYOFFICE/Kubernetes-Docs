5ed58ea6e3d3bbd37bd4d049c8f8a12ade5e2f63
danilapog <danil.titarenko@onlyoffice.com>
2023-04-17 11:07:22 +0300

    Add helm test gh-action

---
+++ sources/scripts/deploy-Kubernetes-Docs.sh
@@ -0,0 +1,120 @@
+#!/usr/bin/env bash
+
+# Scripts for deploys and check Kuberneted-Docs helm chart
+
+set -euo pipefail 
+
+K8S_STORAGE_CLASS="standard"
+NFS_PERSISTANCE_SIZE="8Gi"
+
+export TERM=xterm-256color^M
+
+function common::get_colors() {
+    COLOR_BLUE=$'\e[34m'
+    COLOR_GREEN=$'\e[32m'
+    COLOR_RED=$'\e[31m'
+    COLOR_RESET=$'\e[0m'
+    COLOR_YELLOW=$'\e[33m'
+    export COLOR_BLUE
+    export COLOR_GREEN
+    export COLOR_RED
+    export COLOR_RESET
+    export COLOR_YELLOW
+}
+
+function k8s_w8_workers() {
+         for i in {1..20}; do
+            echo "${COLOR_BLUE}🔨⎈ Get k8s workers status ${i}...${COLOR_RESET}"
+            local NODES_STATUS=$(kubectl get nodes -o json | jq -r '.items[] | select ( .status.conditions[] | select( .type=="Ready" and .status=="False")) | .metadata.name')
+            if [[ -z "${NODES_STATUS}" ]]; then
+              echo "${COLOR_GREEN}☑ OK: K8s workers is ready. Continue...${COLOR_RESET}"
+              local k8s_workers_ready='true'
+              break
+            else
+              sleep 5
+            fi
+         done
+         if [[ "${k8s_workers_ready}" != 'true' ]]; then
+           err "\e[0;31m Something goes wrong. k8s is not ready \e[0m"
+           exit 1
+         fi
+}
+
+function k8s_get_info() {
+            echo "${COLOR_BLUE}🔨⎈ Get cluster info...${COLOR_RESET}"
+            kubectl get all
+            kubectl get ns
+            kubectl get sc
+            kubectl get nodes
+}
+
+function k8s_deploy_deps() {
+            echo "${COLOR_BLUE}🔨⎈ Add depends helm repos...${COLOR_RESET}"
+            # Add dependency helm charts
+            helm repo add bitnami https://charts.bitnami.com/bitnami
+            helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+            helm repo add nfs-server-provisioner https://kubernetes-sigs.github.io/nfs-ganesha-server-and-external-provisioner
+            helm repo add onlyoffice https://download.onlyoffice.com/charts/stable
+            helm repo update
+            echo "${COLOR_GREEN}☑ OK: Helm repository was added${COLOR_RESET}" 
+
+            echo "${COLOR_BLUE}🔨⎈ Lets deploy dependency...${COLOR_RESET}"
+            # Install nfs server
+            helm install nfs-server nfs-server-provisioner/nfs-server-provisioner \
+                 --set persistence.enabled=true \
+                 --set persistence.storageClass=${K8S_STORAGE_CLASS} \
+                 --set persistence.size=${NFS_PERSISTANCE_SIZE} > /dev/null 2>&1
+            echo "${COLOR_GREEN}☑ OK: NFS Server was deployed${COLOR_RESET}"
+
+            # Install rabbitmq
+            helm install rabbitmq bitnami/rabbitmq \
+                 --set metrics.enabled=false > /dev/null 2>&1
+            echo "${COLOR_GREEN}☑ OK: Rabbitmq was deployed${COLOR_RESET}"
+
+            # Install redis
+            helm install redis bitnami/redis \
+                 --set architecture=standalone \
+                 --set metrics.enabled=false > /dev/null 2>&1
+            echo "${COLOR_GREEN}☑ OK: Redis was deployed${COLOR_RESET}"
+
+            # Install postgresql
+            helm install postgresql bitnami/postgresql \
+                 --set auth.database=postgres \
+                 --set primary.persistence.size=2G \
+                 --set metrics.enabled=false > /dev/null 2>&1
+            echo "${COLOR_GREEN}☑ OK: Postgresql was deployed${COLOR_RESET}"
+
+     }
+
+function k8s_wait_deps() {
+                echo "${COLOR_BLUE}🔨⎈ Wait Docs dependencies${COLOR_RESET}"
+                sleep 120
+     }
+
+function k8s_ct_install() {
+            echo "${COLOR_YELLOW}⚠ Attention: Start ct install test..${COLOR_RESET}"
+            ct install --chart-dirs . --charts . --helm-extra-set-args "--set=namespaceOverride=default"
+            if [[ "$?" == 0 ]]; then
+               echo "${COLOR_GREEN}👌👌👌⎈ Helm install/test/upgrade successfull finished${COLOR_RESET}"
+               echo "${COLOR_BLUE}🔨⎈ Get test logs...${COLOR_RESET}"
+               kubectl logs -f test-ds --namespace=default
+	       k8s_get_info
+	       exit 0
+            else 
+               echo "${COLOR_RED}🔥Tests failed. Get test logs and exit with 1${COLOR_RESET}"
+               kubectl logs -f test-ds --namespace=default
+	       k8s_get_info
+               exit 1
+            fi
+     }
+
+function main () {
+   common::get_colors
+   k8s_get_info
+   k8s_w8_workers
+   k8s_deploy_deps
+   k8s_wait_deps
+   k8s_ct_install
+ }
+
+main
